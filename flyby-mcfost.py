e1cd1b20cd464cc15daf6f1e23dc7ae75954e01b
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-04-30 13:04:50 +1000

    Flyby project: mostly scripts to post-process with MCFOST and pymcfost

---
+++ flyby-mcfost.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+'''
+flyby-mcfost.py
+D. Mentiplay, 2018.
+
+Run mcfost on flyby models.
+'''
+
+import glob
+import os
+import shutil
+import subprocess
+
+# ---------------------------------------------------------------------------- #
+
+SETUP = True
+RUN = True
+
+PWD = os.path.dirname(os.path.abspath('__file__'))
+CONFIG_DIR = os.path.expandvars('$HOME') + '/repos/phd/projects/dusty-discs/flyby'
+MCFOST_DIR = os.path.expandvars('$MCFOST_DIR')
+DUMP_DIR = os.path.expandvars('$HOME') + '/runs/dusty-discs/flyby'
+
+LIMITS = PWD + '/flyby-limits'
+MCFOST = PWD + '/mcfost'
+
+BETA = ['45', '135']
+TIME = ['100', '110', '120', '150']
+INCLINATION = ['00', '45', '90']
+WAVELENGTH = ['1.6', '850', '2100']
+MOLECULE = ['CO']
+
+# ---------------------------------------------------------------------------- #
+
+if SETUP:
+
+    shutil.copy2(CONFIG_DIR + '/flyby-limits', PWD)
+    shutil.copy2(MCFOST_DIR + '/src/mcfost', PWD)
+
+    for inclination in INCLINATION:
+        paras = [CONFIG_DIR + '/flyby-temperature.para',
+                 CONFIG_DIR + '/flyby-alma-i' + inclination + '.para',
+                 CONFIG_DIR + '/flyby-scat-i' + inclination + '.para']
+        for para in paras:
+            shutil.copy2(para, PWD)
+
+    for beta in BETA:
+        for time in TIME:
+
+            for inclination in INCLINATION:
+                DIR = PWD + '/b' + beta + '/t' + time + '/i' + inclination
+                os.makedirs(DIR)
+
+            dump = DUMP_DIR + '/b' + beta + '-' + time + '_'
+            shutil.copy2(dump, PWD)
+
+# ---------------------------------------------------------------------------- #
+
+if RUN:
+
+    if not os.path.isfile(MCFOST):
+        raise FileNotFoundError
+
+    for beta in BETA:
+        for time in TIME:
+
+            DUMP_FILE = 'b' + beta + '-' + time + '_'
+            DUMP = DUMP_DIR + '/' + DUMP_FILE
+
+            print(3*'\n')
+            print('+' + 20*'=' + '+', flush=True)
+            print('|' + 20*' ' + '|', flush=True)
+            print(f'|  DUMP = {DUMP_FILE:9}  |', flush=True)
+            print('|' + 20*' ' + '|', flush=True)
+            print('|' + 20*' ' + '|', flush=True)
+            print('+' + 20*'=' + '+', flush=True)
+            print(3*'\n')
+
+            # --- temperature --- #
+
+            PARA_FILE = 'flyby-temperature.para'
+            PARA = PWD + '/' + PARA_FILE
+
+            message = 'Calculating temperature'
+            print(3*'\n' + '---- ' + message + ' ----' + 3*'\n', flush=True)
+
+            LOG_FILE = DUMP_FILE + 'th.log'
+            LOG = PWD + '/' + LOG_FILE
+            ROOT_DIR = 'b' + beta + '/t' + time
+
+            MCFOST_COMMAND = MCFOST + ' ' + PARA + ' -phantom ' + DUMP \
+                             + ' -limits ' + LIMITS + ' | tee ' + LOG
+
+            subprocess.run(MCFOST_COMMAND, shell=True)
+
+            # --- thermal emission / scattered light --- #
+
+            for inclination in INCLINATION:
+
+                message = 'Inclination is ' + inclination + ' degrees'
+                print(3*'\n' + '---- ' + message + ' ----' + 3*'\n', flush=True)
+
+                DIR_INC = ROOT_DIR + '/i' + inclination
+
+                for wavelength in WAVELENGTH:
+
+                    message = 'Calculating image at ' + wavelength + ' µm'
+                    print(3*'\n' + '---- ' + message + ' ----' + 3*'\n', flush=True)
+
+                    LOG_FILE = DUMP_FILE + 'i' + inclination + '_' + wavelength + '.log'
+                    LOG = PWD + '/' + LOG_FILE
+
+                    if float(wavelength) < 10.:
+                        PARA='flyby-scat-i' + inclination + '.para'
+                        CASA=''
+                        IGNORE_DUST='-ignore_dust'
+                    else:
+                        PARA='flyby-scat-i' + inclination + '.para'
+                        CASA='-casa'
+                        IGNORE_DUST=''
+
+                    MCFOST_COMMAND = MCFOST + ' ' + PARA + ' -phantom ' + DUMP \
+                                     + ' -limits ' + LIMITS + ' -img ' \
+                                     + wavelength + ' ' + CASA + ' ' \
+                                     + IGNORE_DUST + ' | tee ' + LOG
+
+                    subprocess.run(MCFOST_COMMAND, shell=True)
+
+                    shutil.move('data_' + wavelength, DIR_INC)
+
+            shutil.move('data_th', ROOT_DIR)
+
+            # --- molecular emission --- #
+
+            for inclination in INCLINATION:
+
+                message = 'Inclination is ' + inclination + ' degrees'
+                print(3*'\n' + '---- ' + message + ' ----' + 3*'\n', flush=True)
+
+                DIR_INC = ROOT_DIR + '/i' + inclination
+
+                for molecule in MOLECULE:
+
+                    message = 'Calculating ' + molecule + ' emission'
+                    print(3*'\n' + '---- ' + message + ' ----' + 3*'\n', flush=True)
+
+                    LOG_FILE = DUMP_FILE + 'i' + inclination + '_' + molecule + '.log'
+                    LOG = PWD + '/' + LOG_FILE
+
+                    PARA='flyby-alma-i' + inclination + '.para'
+                    CASA='-casa'
+                    IGNORE_DUST='-ignore_dust'
+
+                    MCFOST_COMMAND = MCFOST + ' ' + PARA + ' -phantom ' + DUMP \
+                                   + ' -limits ' + LIMITS + ' -mol ' + CASA + ' ' \
+                                   + IGNORE_DUST + ' | tee ' + LOG
+
+                    subprocess.run(MCFOST_COMMAND, shell=True)
+
+                    shutil.move('data_' + molecule, DIR_INC)
+                    shutil.move('data_th', DIR_INC + '/data_th_' + molecule)
+
+                    for file in glob.glob(r'*.tmp'):
+                        shutil.move(file, DIR_INC + '/data_th_' + molecule)
---
+++ flyby_figures.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+'''
+flyby_figures.py
+
+D. Mentiplay, 2018.
+
+Plot flyby model data from Cuello+2018 (https://arxiv.org/abs/1812.00961)
+
+Relies on pymcfost (https://github.com/cpinte/pymcfost), and my module
+pymcfost_subplots.py (~phd/projects/dusty-discs/flyby/pymcfost_subplots.py).
+
+Assumes data directory structure in the current directory is:
+
+    .
+    ├── b1
+    │   ├── t1
+    │   │   ├── i1
+    │   │   │   ├── data_1
+    │   │   │   ├── data_2
+    │   │   ├── i2
+    │   │   │   ├── data_1
+    │   │   │   ├── data_2
+    ├── b2
+    │   ├── t1
+    │   │   ├── i1
+    │   │   │   ├── data_1
+    │   │   │   ├── data_2
+    │   │   ├── i2
+    │   │   │   ├── data_1
+    │   │   │   ├── data_2
+'''
+
+import os
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+import pymcfost as mcfost
+from pymcfost_subplots import plot_figure, OptionsImage, OptionsLine
+
+################################################################################
+############     SET OPTIONS     ###############################################
+################################################################################
+
+DO_THERMAL   = True             # plot thermal emission
+DO_SCATTERED = True             # plot scattered light
+DO_LINES     = True             # plot CO emission
+
+SAVEFIG = True                  # save figures to pdf
+DROPBOX = True                  # cp files to dropbox
+
+SCALE        = 5.               # figure size
+FONT_SCALING = 4.               # font size
+
+BETAS        = ['45', '135']                  # angle of flyby
+TIMES        = ['100', '110', '120', '150']   # time of flyby (100 is periastron)
+INCLINATIONS = ['00', '45', '90']             # inclination w.r.t. observer
+MOMENTS      = [0, 1, 2]                      # molecular emission moments
+
+SCATTERED = ['1.6']
+THERMAL   = ['850', '2100']
+MOLECULAR = ['CO']
+
+radiations = list()
+
+if DO_THERMAL:
+    radiations += THERMAL
+
+if DO_SCATTERED:
+    radiations += SCATTERED
+
+if DO_LINES:
+    radiations += MOLECULAR
+
+opts_thermal = OptionsImage(
+    flux             = 'I',
+    dynamic_range    = None,
+    vmin             = 1e-07,
+    vmax             = 1e-04,
+    fpeak            = None,
+    psf_FWHM         = 0.20,
+    plot_beam        = True,
+    scale            = 'log',
+    cmap             = 'inferno',
+    coronagraph      = None,
+    colorbar_subplot = False,
+    colorbar_figure  = True
+    )
+
+opts_scattered = OptionsImage(
+    flux             = 'I',
+    dynamic_range    = None,
+    vmin             = 1e-19,
+    vmax             = 1e-16,
+    fpeak            = None,
+    psf_FWHM         = 0.05,
+    plot_beam        = True,
+    scale            = 'log',
+    cmap             = 'gist_heat',
+    coronagraph      = None,
+    colorbar_subplot = False,
+    colorbar_figure  = True
+    )
+
+opts_molecular = OptionsLine(
+    #                     M0            M1          M2
+    fmin              =   [0,           -2.5,       0],
+    fmax              =   [0.002,       2.5,        5],
+    psf_FWHM          =   [0.20,        None,       None],
+    plot_beam         =   [True,        None,       None],
+    color_scale       =   [None,        None,       None],
+    cmap              =   ['Blues_r',   'RdBu',     'viridis'],
+    colorbar_subplot  =   [False,       False,      False],
+    colorbar_figure   =   [True,        True,       True]
+    )
+
+################################################################################
+
+opts = {'thermal':   opts_thermal,
+        'scattered': opts_scattered,
+        'molecular': opts_molecular}
+
+mpl.rcParams['font.family'] = 'serif'
+mpl.rcParams['font.size'] = FONT_SCALING*SCALE
+
+position_labels = ['top_left', 'top_right', 'bottom_left', 'bottom_right']
+positions = dict()
+positions['top_left']     = (0.05, 0.90)
+positions['top_right']    = (0.95, 0.90)
+positions['bottom_left']  = (0.05, 0.10)
+positions['bottom_right'] = (0.95, 0.10)
+
+text = dict()
+for time in TIMES:
+    text[time] = dict()
+    for inclination in INCLINATIONS:
+        text[time][inclination] = dict()
+        for position in position_labels:
+            text[time][inclination][position] = None
+        if inclination == INCLINATIONS[0]:
+            text[time][inclination]['top_right'] = 't = ' + time
+        if time == TIMES[0]:
+            text[time][inclination]['top_left'] = 'i = ' + inclination + r'$\degree$'
+
+for beta in BETAS:
+
+    print(f'=== beta = {beta} ===', flush=True)
+
+    for radiation in radiations:
+
+        print(f'   --- radiation = {radiation} ---', flush=True)
+
+        if radiation in THERMAL:
+            itype = 'thermal'
+            data = 'Image'
+
+        elif radiation in SCATTERED:
+            itype = 'scattered'
+            data = 'Image'
+
+        elif radiation in MOLECULAR:
+            itype = 'molecular'
+            data = 'Line'
+
+        else:
+            raise ValueError(f'{radiation} not in thermal, scattered, molecular lists')
+
+        images = dict()
+
+        for time in TIMES:
+            images[time] = dict()
+
+            for inclination in INCLINATIONS:
+                data_directory = 'b' + beta + '/t' + time + '/i' + inclination \
+                               + '/data_' + radiation
+
+                if data == 'Image':
+                    images[time][inclination] = mcfost.Image(data_directory)
+
+                elif data == 'Line':
+                    images[time][inclination] = mcfost.Line(data_directory)
+
+
+        options = opts[itype]
+
+        if data == 'Image':
+
+            figure, axes = plot_figure(data=data, images=images, options=options,
+                                       text=text, positions=positions)
+
+            filename = itype + '_b' + beta + '_' + radiation + '.pdf'
+            figures = [figure]
+            filenames = [filename]
+
+        elif data == 'Line':
+
+            figures = []
+            filenames = []
+
+            for moment in MOMENTS:
+                figure, axes = plot_figure(data=data, images=images, options=options,
+                                           moment=moment, text=text, positions=positions)
+
+                filename = itype + '_b' + beta + '_' + radiation + '_m' + str(moment) + '.pdf'
+                figures.append(figure)
+                filenames.append(filename)
+
+        if SAVEFIG:
+
+            for idx, figure in enumerate(figures):
+                filename = filenames[idx]
+                figure.savefig(filename, bbox_inches='tight')
+
+                if DROPBOX:
+                    cmd = f'\\cp {filename} ~/Dropbox/swap'
+                    try:
+                        os.system(cmd)
+                        print(f'      copying {filename} to ~/Dropbox/swap', flush=True)
+                    except OSError:
+                        print("Can't copy pdfs to ~/Dropbox/swap", flush=True)
+
+            plt.close('all')
